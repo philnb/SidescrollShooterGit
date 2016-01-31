1987a20e321d8f8a165c9e4e37052992708e3af2
Philipp Goelter <phil.g992@googlemail.com>
2016-01-31 22:34:13 +0100

    No Comment

--- SidescrollShooter/Assets/Scripts/PlaySoundOnAction.cs
+++ SidescrollShooter/Assets/Scripts/PlaySoundOnAction.cs
@@ -5,25 +5,34 @@ public class PlaySoundOnAction : MonoBehaviour {
 
 	public bool onDestroy 	= false;
 	public bool onKeyStroke = false;
-	public KeyCode keySound;
+	public KeyCode key;
 
 	// Use this for initialization
 	void Start () 
 	{
+		// This because audioclip immediately launched at start.
 		GetComponent<AudioSource>().Stop();
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+		/**
+		 * TODO: Does not work yet.
+		 * Should play a sound when a gameobject is destroyed.
+		 **/
 		if(onDestroy && gameObject == null)
 		{
 			gameObject.GetComponent<AudioSource>().Play();
 		}
 
+		/**
+		 * By pressing a key (specified in variable: key), plays the AudioSource
+		 * that is attached to the same object this script is attached.
+		 **/
 		if(onKeyStroke)
 		{
-			if(Input.GetKey(keySound))
+			if(Input.GetKey(key))
 				gameObject.GetComponent<AudioSource>().Play();
 		}
 	
