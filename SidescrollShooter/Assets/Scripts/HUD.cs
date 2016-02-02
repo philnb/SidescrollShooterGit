4500f470a46f9bdd49df9e1ec5ec6b5f1a29054b
Phil <phil.g992@googlemail.com>
2016-02-02 15:55:48 +0100

    HUD added

---
+++ SidescrollShooter/Assets/Scripts/HUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{
+    int score;
+	// Use this for initialization
+	void Start ()
+    {
+        score = 44;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 200, 50), "Score: " + score);
+    }
+}
